499b1edc0503aace6d75d27de9751e9fb55cfe36
Tungbo <wangyongxiang@vspn.cn>
2022-06-01 19:12:55 +0800

    初始化项目

---
+++ android/app/src/main/kotlin/com/tungbo/fultter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tungbo.fultter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/test.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:fultter/main.dart';
+
+void main() {
+  runApp(const MyApp2());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp() {
+    Future.delayed(Duration(seconds: 3)).then((s) => {this.data = "tungbo"});
+  }
+
+  String data = "wyx";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("haha"),
+            ),
+            body: Center(child: Text(data))));
+  }
+}
+
+class MyApp2 extends StatefulWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp2> createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  String data = "wyx";
+
+  //构造函数不能setState()
+  //build --> Future
+  _MyApp2State() {
+    //Future Event队列
+    Future.delayed(Duration(microseconds: 1)).then((s) {
+      debugPrint("Future");
+      setState(() {
+        this.data = "tungbo11";
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("build");
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("haha"),
+            ),
+            body: Center(child: Text(data))));
+  }
+}
