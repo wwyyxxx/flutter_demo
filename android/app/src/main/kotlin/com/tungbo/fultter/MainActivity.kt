57ea11741656b4ee194da75464e5e8b9fb584026
Tungbo <wangyongxiang@vspn.cn>
2022-06-07 16:01:39 +0800

    1.启动页设置 2.完善readme

--- android/app/src/main/kotlin/com/tungbo/fultter/MainActivity.kt
+++ android/app/src/main/kotlin/com/tungbo/fultter/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.tungbo.fultter
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import org.devio.flutter.splashscreen.SplashScreen
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this, true)// here
+        super.onCreate(savedInstanceState)
+    }
 }
